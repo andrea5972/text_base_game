854080b62099707a364726934ee682937acaab4b
Andrea Murphy <andrealindamurphy@gmail.com>
2018-11-26 19:32:41 -0500

    add enemies

--- Zantar/src/zantar/Enemy.java
+++ Zantar/src/zantar/Enemy.java
@@ -5,7 +5,22 @@
 
 package zantar;
 
+import java.util.Random;
+
 public class Enemy {
+	public static final Random RANDOM = new Random(); 
+	
+   // An array of possible enemy types
+	public static final String[] ENEMY_NAMES = { "Troll", "Chipmunk", "Zombie", "Rabit", "Warrior", "Goblin" };
+
+    public static final int max_attack_damage = 20;
+   
+    public static final int max_health = 75;
+
+    public static final int min_health = 1;
+
+    private int health;
+    private String name;
 
 	public int attack() {
 		// TODO Auto-generated method stub
